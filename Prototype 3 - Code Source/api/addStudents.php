b0b2087f65e111b7567c8b2087086e4df515a3ab
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-06-03 13:04:00 +0100

    Save Changes

--- Prototype 3 - Code Source/api/addStudents.php
+++ Prototype 3 - Code Source/api/addStudents.php
@@ -1,6 +1,6 @@
 <?php
 $dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","12345");
-$sql = " INSERT INTO schoolstudents(name,fathername,rollno,degree,branch) VALUES (:name,:fathername,:rollno,:degree,:branch)";
+$sql = "INSERT INTO schoolstudents(name,fathername,rollno,degree,branch) VALUES (:name,:fathername,:rollno,:degree,:branch)";
 $addStudentsQuery = $dbh->prepare($sql);
 $addStudentsQuery->bindParam(":name",$_POST["name"],PDO::PARAM_STR);
 $addStudentsQuery->bindParam(":fathername",$_POST["fathername"],PDO::PARAM_STR);
