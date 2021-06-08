ccdfc783bb711669129ce1a8428c0d1182dbbf77
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-06-08 11:55:19 +0100

    Réalisation Terminer

--- Réalisation/api/editFormer.php
+++ Réalisation/api/editFormer.php
@@ -1,12 +1,12 @@
 <?php
-$id = $_POST["sid"];
-$dbh = new PDO("mysql:host=localhost;dbname=schoolstd","root","12345");
+$id = $_POST["id"];
+$dbh = new PDO("mysql:host=localhost;dbname=Formers","root","12345");
 $sql = "UPDATE Former SET Firstname = :Firstname,Lastname = :Lastname,Matricule = :Matricule,Email = :Email WHERE id = $id";
-$addStudentsQuery = $dbh->prepare($sql);
-$addStudentsQuery->bindParam(":name",$_POST["name"],PDO::PARAM_STR);
-$addStudentsQuery->bindParam(":fathername",$_POST["fathername"],PDO::PARAM_STR);
-$addStudentsQuery->bindParam(":rollno",$_POST["rollno"],PDO::PARAM_STR);
-$addStudentsQuery->bindParam(":degree",$_POST["degree"],PDO::PARAM_STR);
-$addStudentsQuery->bindParam(":branch",$_POST["branch"],PDO::PARAM_STR);
-$addStudentsQuery->execute();
+$addFormerQuery = $dbh->prepare($sql);
+$addFormerQuery->bindParam(":Firstname",$_POST["Firstname"],PDO::PARAM_STR);
+$addFormerQuery->bindParam(":Lastname",$_POST["Lastname"],PDO::PARAM_STR);
+$addFormerQuery->bindParam(":Matricule",$_POST["Matricule"],PDO::PARAM_STR);
+$addFormerQuery->bindParam(":Email",$_POST["Email"],PDO::PARAM_STR);
+
+$addFormerQuery->execute();
 ?>
